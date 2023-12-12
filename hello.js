1021faad98350c3d4b36f2068b60b8fd173ac310
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-12-12 12:54:09 +0600

    operators

--- hello.js
+++ hello.js
@@ -6,3 +6,20 @@ let c = a+b
 console.log(a+ "+" +b+ "=" +c)
 console.log(a+ "+" +b+ "=" +(a+b))
 console.log(c)
+
+
+let ab =10;
+
+// console.log(ab+=15);
+console.log(ab-=15);
+
+let x= 10,y= 5;
+
+console.log(x<y)
+
+let l = 50, m = 200;
+console.log(l>20 || m<100)
+
+let k =0;
+
+console.log(++k)
