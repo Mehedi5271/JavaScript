cadb9cb5859688b3433a15ae23407b0e1ad552a5
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-12-13 23:39:02 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,22 @@
+let boro_vai = 45
+let choto_vai = 10
+
+if(boro_vai>choto_vai){
+    console.log('boro vai boro')
+}
+else{
+    console.log('choto vai e boro')
+}
+
+
+let day =0;
+
+switch(day){
+    case 0:
+        console.log("Sat");
+        break
+        case 1:
+
+    console.log("sun");
+}
+
